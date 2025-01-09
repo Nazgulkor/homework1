80a0a31d7c0a6f492ff248e4e1150a45283ccb84
relapse having <usupmezhiev95@gmail.com>
2025-01-09 09:56:10 +0300

    added MyArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,16 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Homework.turnString("i love java");
-
-        Homework.getDistinctNumbers(new int[]{1, 2, 2, 3, 4, 5, 5, 6, 7, 8, 8, 9});
-
-        System.out.println(Homework.findSecondMaxElement(new int[]{10, 15, 23, 11, 44, 13, 66, 1, 6, 47}));
-
-        System.out.println(Homework.lengthOfLastWord("Hello world"));
-        System.out.println(Homework.lengthOfLastWord("    fly me    to the moon    "));
-
-        System.out.println(Homework.isPalindrome("abc"));
-        System.out.println(Homework.isPalindrome("112233"));
-        System.out.println(Homework.isPalindrome("aba"));
-        System.out.println(Homework.isPalindrome("112211"));
+        MyArrayList<String> cars = new MyArrayList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        cars.add(1, "weq");
+        System.out.println(cars.get(1));
     }
 }
